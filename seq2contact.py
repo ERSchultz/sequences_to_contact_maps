d396bdf3ec9bac8b23188441bb97b504e52d320d
ERSchultz <schultz-eric@hotmail.com>
2021-04-07 19:25:14 -0500

    update

--- seq2contact.py
+++ seq2contact.py
@@ -143,4 +143,4 @@ def main(dir, epochs = 1000, device = 'cuda:0', k = 2):
 if __name__ == '__main__':
     clusterdir = '../../../project2/depablo/erschultz/dataset_04_06_21'
     mydir = 'dataset_04_06_21'
-    main(mydir, 50)
+    main(clusterdir, 50)
