a5bf5dc7d093b8f37fdff636ea3337500dac17f6
ERSchultz <schultz-eric@hotmail.com>
2021-04-28 13:10:02 -0500

    updates

--- plotting_script.py
+++ plotting_script.py
@@ -35,8 +35,8 @@ def contactPlots(dataFolder):
         y_prcnt_norm = np.load(os.path.join(path, 'y_prcnt_norm.npy'))
         plotContactMap(y_prcnt_norm, os.path.join(path, 'y_prcnt_norm.png'), title = 'prcnt normalization', vmax = 'max', prcnt = True)
 
-def plot_predictions(dataFolder, modelName):
-    seq2ContactData = Sequences2Contacts(dataFolder, toxx = True,
+def plot_predictions(dataFolder, modelName, y_norm):
+    seq2ContactData = Sequences2Contacts(dataFolder, toxx = True, y_norm = y_norm,
                                         names = True, min_sample = 0)
     dataloader, _, _ = getDataLoaders(seq2ContactData, batch_size = 1,
                                             num_workers = 0, seed = 42,
@@ -50,22 +50,24 @@ def plot_predictions(dataFolder, modelName):
         model.load_state_dict(save_dict['model_state_dict'])
         model.eval()
     vmax = 'mean'
-    for (x, y, name) in dataloader:
+    for (x, y, path) in dataloader:
+        print(path)
+        name = os.path.split(name[0])[-1]
         print(name)
-        name = name[0].split('/')[1] # TODO use os.path.split
         yhat = model(x)
-        plotContactMap(y, os.path.join(dataFolder, name, '_y.png'), vmax = vmax, title = 'Y')
-        plotContactMap(yhat.detach().numpy(), os.path.join(dataFolder, name, '_yhat.png'), vmax = vmax, title = 'Y hat')
+        break
+        plotContactMap(y, os.path.join(path, 'y.png'), vmax = vmax, title = 'Y')
+        plotContactMap(yhat.detach().numpy(), os.path.join(path, 'yhat.png'), vmax = vmax, title = 'Y hat')
         ydif = yhat.detach().numpy() - y.numpy()
-        plotContactMap(ydif, os.path.join(dataFolder, name, '_ydif.png'), vmax = vmax, title = 'difference')
+        plotContactMap(ydif, os.path.join(path, 'ydif.png'), vmax = vmax, title = 'difference')
         loss = F.mse_loss(yhat, y)
         print(loss)
 
 def main():
-    freqDistributionPlots('dataset_04_18_21')
-    freqStatisticsPlots('dataset_04_18_21')
-    contactPlots('dataset_04_18_21')
-    # plot_predictions('dataset_04_18_21', None)
+    # freqDistributionPlots('dataset_04_18_21')
+    # freqStatisticsPlots('dataset_04_18_21')
+    # contactPlots('dataset_04_18_21')
+    plot_predictions('dataset_04_18_21', 'UNet_nEpochs15_nf8_lr0.1_milestones5-10_yNormdiag.pt', 'diag')
 
 
 if __name__ == '__main__':
