336d51a0056e7b62c36371ba54421e1794cdb840
Eric Schultz <erschultz@uchicagoe.edu>
2021-05-11 10:33:23 -0500

    update

--- DeepC.py
+++ DeepC.py
@@ -3,11 +3,31 @@ import torch.nn.functional as F
 import torch.optim as optim
 from neural_net_utils.networks import DeepC
 from neural_net_utils.dataset_classes import Sequences2Contacts
-from neural_net_utils.utils import argparseSetup
+from neural_net_utils.utils import argparseSetup, str2list
 from neural_net_utils.core_test_train import core_test_train
 
 def main():
     opt = argparseSetup()
+    # architecture
+    opt.mode = 'testing'
+    if opt.mode == 'testing':
+        opt.k=2
+        opt.n=1024
+        opt.y_preprocessing='diag'
+        opt.y_norm='batch'
+        opt.kernel_w_list=str2list('5-5-5')
+        opt.hidden_sizes_list=str2list('32-64-96')
+        opt.dilation_list=str2list('2-4-8-16-32-64-128')
+        opt.hidden_size_dilation=96
+
+        # hyperparameters
+        opt.n_epochs=2
+        opt.lr=0.1
+        opt.batch_size=4
+        opt.numWorkers=4
+        opt.milestones='1'
+        opt.gamma=0.1
+
     print(opt)
 
     # Set up model
