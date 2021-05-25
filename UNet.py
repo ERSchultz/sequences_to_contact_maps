aefc4657a616937c4b87aee3bbd96e0b0875f579
ERSchultz <schultz-eric@hotmail.com>
2021-05-24 22:56:25 -0500

    update

--- UNet.py
+++ UNet.py
@@ -13,7 +13,7 @@ import argparse
 
 def main():
     opt = argparseSetup()
-    opt.mode = 'debugging'
+    # opt.mode = 'debugging'
     if opt.mode == 'debugging':
         opt.mode = 'real'
         # Preprocessing
