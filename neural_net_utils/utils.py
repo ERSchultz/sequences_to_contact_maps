eb42125894bad434e3094e95c499906500d2a2d3
ERSchultz <schultz-eric@hotmail.com>
2021-05-27 15:14:45 -0500

    update

--- neural_net_utils/utils.py
+++ neural_net_utils/utils.py
@@ -446,6 +446,8 @@ def argparseSetup():
         assert os.path.exists(txt_file), "{} does not exist".format(txt_file)
         print(sys.argv)
         print(sys.argv[0])
+        print(os.getcwd())
+        return
         opt = parser.parse_args(sys.argv.append('@{}'.format(txt_file))) # parse again
         print(opt.id, opt.model_type)
 
