8554676aa7dca5f0187cfb9f4e2d3b54dbdaeb7f
ERSchultz <schultz-eric@hotmail.com>
2021-08-06 14:07:22 -0500

    new model

--- bashFiles/ContactGNN2.sh
+++ bashFiles/ContactGNN2.sh
@@ -20,7 +20,7 @@ outputMode='sequence'
 k=2
 m=1024
 yPreprocessing='diag'
-yNorm='instance'
+yNorm='none'
 yLogTransform='false'
 messagePassing='GCN'
 useNodeFeatures='false'
@@ -30,7 +30,7 @@ transforms='constant'
 preTransforms='none'
 topK='none'
 sparsifyThresholdUpper='none'
-sparsifyThreshold=0.0
+sparsifyThreshold=1.0
 loss='BCE'
 outAct='none'
 headArchitecture='none'
@@ -51,7 +51,7 @@ relabel_11_to_00='true'
 cd ~/sequences_to_contact_maps
 source activate python3.8_pytorch1.8.1_cuda10.2
 
-for yNorm in 'instance' 'none'
+for preTransforms in 'degree' 'weighted_degree'
 do
   for lr in 1e-1 1e-2 1e-3
   do
@@ -59,20 +59,3 @@ do
   done
   python3 cleanDirectories.py --data_folder $dirname --root_name $rootName --use_scratch $useScratch
 done
-
-# sparsifyThreshold='none'
-# sparsifyThresholdUpper=1.5
-# for lr in 1e-2 1e-3
-# do
-#   python3 core_test_train.py --data_folder $dirname --root_name $rootName --delete_root $deleteRoot --model_type $modelType --GNN_mode $GNNMode --output_mode $outputMode --k $k --m $m --y_preprocessing ${yPreprocessing} --y_norm $yNorm --y_log_transform $yLogTransform --message_passing $messagePassing --use_node_features $useNodeFeatures --use_edge_weights $useEdgeWeights --hidden_sizes_list $hiddenSizesList --transforms $transforms --pre_transforms $preTransforms --top_k $topK --sparsify_threshold $sparsifyThreshold --sparsify_threshold_upper $sparsifyThresholdUpper --loss $loss --out_act $outAct --head_architecture $headArchitecture --head_hidden_sizes_list $headHiddenSizesList --n_epochs $nEpochs --lr $lr --batch_size $batchSize --num_workers $numWorkers --milestones $milestones --gamma $gamma --verbose $verbose --use_scratch $useScratch --plot_predictions $plotPredictions --relabel_11_to_00 $relabel_11_to_00
-# done
-# python3 cleanDirectories.py --data_folder $dirname --root_name $rootName --use_scratch $useScratch
-#
-# sparsifyThreshold=0.5
-# sparsifyThresholdUpper='none'
-# useEdgeWeights='false'
-# for lr in 1e-1
-# do
-#   python3 core_test_train.py --data_folder $dirname --root_name $rootName --delete_root $deleteRoot --model_type $modelType --GNN_mode $GNNMode --output_mode $outputMode --k $k --m $m --y_preprocessing ${yPreprocessing} --y_norm $yNorm --y_log_transform $yLogTransform --message_passing $messagePassing --use_node_features $useNodeFeatures --use_edge_weights $useEdgeWeights --hidden_sizes_list $hiddenSizesList --transforms $transforms --pre_transforms $preTransforms --top_k $topK --sparsify_threshold $sparsifyThreshold --sparsify_threshold_upper $sparsifyThresholdUpper --loss $loss --out_act $outAct --head_architecture $headArchitecture --head_hidden_sizes_list $headHiddenSizesList --n_epochs $nEpochs --lr $lr --batch_size $batchSize --num_workers $numWorkers --milestones $milestones --gamma $gamma --verbose $verbose --use_scratch $useScratch --plot_predictions $plotPredictions --relabel_11_to_00 $relabel_11_to_00
-# done
-# python3 cleanDirectories.py --data_folder $dirname --root_name $rootName --use_scratch $useScratch
