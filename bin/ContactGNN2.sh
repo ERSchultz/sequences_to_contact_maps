77e41300ff6cf9d900b80487cbff2a146237aa0d
Eric Schultz <erschultz@uchicagoe.edu>
2021-08-24 10:54:42 -0500

    update

--- bin/ContactGNN2.sh
+++ bin/ContactGNN2.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=ContactGNN2
 #SBATCH --output=logFiles/ContactGNN2.out
 #SBATCH --time=20:00:00
-#SBATCH --partition=depablo-gpu
+#SBATCH --partition=gpu2
 #SBATCH --gres=gpu:1
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=4
@@ -51,9 +51,11 @@ relabel_11_to_00='false'
 cd ~/sequences_to_contact_maps
 source activate python3.8_pytorch1.8.1_cuda10.2
 
-for preTransforms in 'degree' 'weighted_degree'
+# 'weighted_degree'
+# 'degree' 1e-2
+for preTransforms in 'degree'
 do
-  for lr in 1e-1 1e-2 1e-3
+  for lr in 1e-1 1e-2
   do
     python3 core_test_train.py --data_folder $dirname --root_name $rootName --delete_root $deleteRoot --model_type $modelType --GNN_mode $GNNMode --output_mode $outputMode --k $k --m $m --y_preprocessing ${yPreprocessing} --y_norm $yNorm --y_log_transform $yLogTransform --message_passing $messagePassing --use_node_features $useNodeFeatures --use_edge_weights $useEdgeWeights --hidden_sizes_list $hiddenSizesList --transforms $transforms --pre_transforms $preTransforms --top_k $topK --sparsify_threshold $sparsifyThreshold --sparsify_threshold_upper $sparsifyThresholdUpper --loss $loss --out_act $outAct --head_architecture $headArchitecture --head_hidden_sizes_list $headHiddenSizesList --n_epochs $nEpochs --lr $lr --batch_size $batchSize --num_workers $numWorkers --milestones $milestones --gamma $gamma --verbose $verbose --use_scratch $useScratch --plot_predictions $plotPredictions --relabel_11_to_00 $relabel_11_to_00
   done
