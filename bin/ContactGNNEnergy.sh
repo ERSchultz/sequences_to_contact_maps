f3c0ef7745f56410d178a69877baab66ccf0e597
Eric Schultz <erschultz@uchicagoe.edu>
2021-08-30 17:03:19 -0500

    update

--- bin/ContactGNNEnergy.sh
+++ bin/ContactGNNEnergy.sh
@@ -34,7 +34,7 @@ sparsifyThresholdUpper='none'
 sparsifyThreshold=0.176
 loss='mse'
 outAct='relu'
-headArchitecture='fc'
+headArchitecture='avg'
 headHiddenSizesList='4-1'
 
 # hyperparameters
@@ -52,11 +52,14 @@ relabel_11_to_00='false'
 cd ~/sequences_to_contact_maps
 source activate python3.8_pytorch1.8.1_cuda10.2
 
-for preTransforms in 'degree'
+for headArchitecture in 'avg' 'concat'
 do
-  for lr in 1e-1 1e-2 1e-3
+  for preTransforms in 'degree'
   do
-    python3 core_test_train.py --data_folder $dirname --root_name $rootName --delete_root $deleteRoot --model_type $modelType --GNN_mode $GNNMode --output_mode $outputMode --k $k --m $m --y_preprocessing ${yPreprocessing} --y_norm $yNorm --y_log_transform $yLogTransform --message_passing $messagePassing --use_node_features $useNodeFeatures --use_edge_weights $useEdgeWeights --hidden_sizes_list $hiddenSizesList --transforms $transforms --pre_transforms $preTransforms --split_neg_pos_edges_for_feature_augmentation $split_neg_pos_edges_for_feature_augmentation --top_k $topK --sparsify_threshold $sparsifyThreshold --sparsify_threshold_upper $sparsifyThresholdUpper --loss $loss --out_act $outAct --head_architecture $headArchitecture --head_hidden_sizes_list $headHiddenSizesList --n_epochs $nEpochs --lr $lr --batch_size $batchSize --num_workers $numWorkers --milestones $milestones --gamma $gamma --verbose $verbose --use_scratch $useScratch --plot_predictions $plotPredictions --relabel_11_to_00 $relabel_11_to_00
+    for lr in 1e-1 1e-2 1e-3
+    do
+      python3 core_test_train.py --data_folder $dirname --root_name $rootName --delete_root $deleteRoot --model_type $modelType --GNN_mode $GNNMode --output_mode $outputMode --k $k --m $m --y_preprocessing ${yPreprocessing} --y_norm $yNorm --y_log_transform $yLogTransform --message_passing $messagePassing --use_node_features $useNodeFeatures --use_edge_weights $useEdgeWeights --hidden_sizes_list $hiddenSizesList --transforms $transforms --pre_transforms $preTransforms --split_neg_pos_edges_for_feature_augmentation $split_neg_pos_edges_for_feature_augmentation --top_k $topK --sparsify_threshold $sparsifyThreshold --sparsify_threshold_upper $sparsifyThresholdUpper --loss $loss --out_act $outAct --head_architecture $headArchitecture --head_hidden_sizes_list $headHiddenSizesList --n_epochs $nEpochs --lr $lr --batch_size $batchSize --num_workers $numWorkers --milestones $milestones --gamma $gamma --verbose $verbose --use_scratch $useScratch --plot_predictions $plotPredictions --relabel_11_to_00 $relabel_11_to_00
+    done
+    python3 cleanDirectories.py --data_folder $dirname --root_name $rootName --use_scratch $useScratch
   done
-  python3 cleanDirectories.py --data_folder $dirname --root_name $rootName --use_scratch $useScratch
 done
