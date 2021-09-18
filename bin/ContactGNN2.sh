c1be9eb5ab948298d09b8c10310ae592526f722e
ERSchultz <schultz-eric@hotmail.com>
2021-09-18 11:46:44 -0500

    updates

--- bin/ContactGNN2.sh
+++ bin/ContactGNN2.sh
@@ -8,7 +8,7 @@
 #SBATCH --ntasks-per-node=4
 #SBATCH --mem-per-cpu=2000
 
-dirname="/project2/depablo/erschultz/dataset_08_18_21"
+dirname="/project2/depablo/erschultz/dataset_04_18_21"
 deleteRoot='false'
 
 modelType='ContactGNN'
@@ -17,7 +17,7 @@ GNNMode='true'
 outputMode='sequence'
 
 # architecture
-k=4
+k=2
 m=1024
 yPreprocessing='diag'
 yNorm='none'
@@ -25,7 +25,7 @@ yLogTransform='false'
 messagePassing='GCN'
 useNodeFeatures='false'
 useEdgeWeights='true'
-hiddenSizesList='16-4'
+hiddenSizesList='16-2'
 transforms='constant'
 preTransforms='none'
 topK='none'
@@ -37,7 +37,7 @@ headArchitecture='none'
 headHiddenSizesList='none'
 
 # hyperparameters
-nEpochs=20
+nEpochs=15
 batchSize=8
 numWorkers=4
 milestones='none'
@@ -55,7 +55,7 @@ source activate python3.8_pytorch1.8.1_cuda10.2
 # 'degree' 1e-2
 for preTransforms in 'degree'
 do
-  for lr in 1e-1 1e-2
+  for lr in 1e-3
   do
     python3 core_test_train.py --data_folder $dirname --root_name $rootName --delete_root $deleteRoot --model_type $modelType --GNN_mode $GNNMode --output_mode $outputMode --k $k --m $m --y_preprocessing ${yPreprocessing} --y_norm $yNorm --y_log_transform $yLogTransform --message_passing $messagePassing --use_node_features $useNodeFeatures --use_edge_weights $useEdgeWeights --hidden_sizes_list $hiddenSizesList --transforms $transforms --pre_transforms $preTransforms --top_k $topK --sparsify_threshold $sparsifyThreshold --sparsify_threshold_upper $sparsifyThresholdUpper --loss $loss --out_act $outAct --head_architecture $headArchitecture --head_hidden_sizes_list $headHiddenSizesList --n_epochs $nEpochs --lr $lr --batch_size $batchSize --num_workers $numWorkers --milestones $milestones --gamma $gamma --verbose $verbose --use_scratch $useScratch --plot_predictions $plotPredictions --relabel_11_to_00 $relabel_11_to_00
   done
