fcb4332dd681d8230831741749c97312499f84b9
ERSchultz <schultz-eric@hotmail.com>
2021-10-05 18:24:40 -0500

    update

--- bin/ContactGNNEnergy2.sh
+++ bin/ContactGNNEnergy2.sh
@@ -8,7 +8,7 @@
 #SBATCH --ntasks-per-node=4
 #SBATCH --mem-per-cpu=2000
 
-dirname="/project2/depablo/erschultz/dataset_08_26_21"
+dirname="/project2/depablo/erschultz/dataset_04_18_21"
 deleteRoot='false'
 
 modelType='ContactGNNEnergy'
@@ -17,7 +17,7 @@ GNNMode='true'
 outputMode='energy'
 
 # architecture
-k=4
+k=2
 m=1024
 yPreprocessing='diag'
 yNorm='none'
@@ -41,7 +41,7 @@ headArchitecture='concat-outer'
 headHiddenSizesList='10-10-1'
 
 # hyperparameters
-nEpochs=100
+nEpochs=60
 batchSize=4
 numWorkers=4
 milestones='none'
