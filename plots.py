e4d25a52e22cf1d37b7e2637fbec2d774215661a
ERSchultz <schultz-eric@hotmail.com>
2022-03-03 17:57:51 -0600

    separated plots from plotting_utils

---
+++ plots.py
@@ -0,0 +1,106 @@
+import csv
+import os.path as osp
+from shutil import rmtree
+
+import numpy as np
+from utils.argparse_utils import (argparse_setup, get_base_parser,
+                                  get_opt_header)
+from utils.plotting_utils import (plot_centroid_distance, plot_combined_models,
+                                  plot_sc_contact_maps, plot_xyz_gif,
+                                  plotting_script)
+
+
+def update_result_tables(model_type = None, mode = None, output_mode = 'contact'):
+    if model_type is None:
+        model_types = ['Akita', 'DeepC', 'UNet', 'GNNAutoencoder', 'GNNAutoencoder2',
+                        'ContactGNN', 'ContactGNNEnergy']
+        modes = [None, None, None, 'GNN', 'GNN']
+        output_modes = ['contact', 'contact', 'contact', 'contact', 'sequence', 'energy']
+    else:
+        model_types = [model_type]
+        modes = [mode]
+        output_modes = [output_mode]
+    for model_type, mode, output_mode in zip(model_types, modes, output_modes):
+        # set up header row
+        opt_list = get_opt_header(model_type, mode)
+        if output_mode == 'contact':
+            opt_list.extend(['Final Validation Loss', 'PCA Accuracy Mean',
+                'PCA Accuracy Std', 'PCA Spearman Mean', 'PCA Spearman Std',
+                'PCA Pearson Mean', 'PCA Pearson Std', 'Overall Pearson Mean',
+                'Overall Pearson Std'])
+        elif output_mode == 'sequence':
+            opt_list.extend(['Final Validation Loss', 'AUC'])
+        elif output_mode == 'energy':
+            opt_list.extend(['Final Validation Loss'])
+        else:
+            raise Exception('Unknown output_mode {}'.format(output_mode))
+        results = [opt_list]
+
+        # get data
+        model_path = osp.join('results', model_type)
+        parser = get_base_parser()
+        for id in range(1, 500):
+            id_path = osp.join(model_path, str(id))
+            if osp.isdir(id_path):
+                txt_file = osp.join(id_path, 'argparse.txt')
+                if osp.exists(txt_file):
+                    opt = parser.parse_args(['@{}'.format(txt_file)])
+                    opt.id = int(id)
+                    opt = finalizeOpt(opt, parser, True)
+                    opt_list = opt2list(opt)
+                    if output_mode == 'contact':
+                        with open(osp.join(id_path, 'PCA_results.txt'), 'r') as f:
+                            f.readline()
+                            acc = f.readline().split(':')[1].strip().split(' +- ')
+                            spearman = f.readline().split(':')[1].strip().split(' +- ')
+                            pearson = f.readline().split(':')[1].strip().split(' +- ')
+                        with open(osp.join(id_path, 'out.log'), 'r') as f:
+                            for line in f:
+                                if line.startswith('Final val loss: '):
+                                    final_val_loss = line.split(':')[1].strip()
+                                elif line.startswith('Overall Pearson R: '):
+                                    dist_pearson = line.split(':')[1].strip().split(' $\pm$ ')
+                        opt_list.extend([final_val_loss, acc[0], acc[1], spearman[0],
+                                        spearman[1], pearson[0], pearson[1],
+                                        dist_pearson[0], dist_pearson[1]])
+                    elif output_mode == 'sequence':
+                        final_val_loss = None; auc = None
+                        with open(osp.join(id_path, 'out.log'), 'r') as f:
+                            for line in f:
+                                if line.startswith('Final val loss: '):
+                                    final_val_loss = line.split(':')[1].strip()
+                                elif line.startswith('AUC: '):
+                                    auc = line.split(':')[1].strip()
+                        opt_list.extend([final_val_loss, auc])
+                    elif output_mode == 'energy':
+                        final_val_loss = None
+                        with open(osp.join(id_path, 'out.log'), 'r') as f:
+                            for line in f:
+                                if line.startswith('Final val loss: '):
+                                    final_val_loss = line.split(':')[1].strip()
+                        opt_list.extend([final_val_loss])
+                    results.append(opt_list)
+
+        ofile = osp.join(model_path, 'results_table.csv')
+        with open(ofile, 'w', newline = '') as f:
+            wr = csv.writer(f)
+            wr.writerows(results)
+
+def main():
+    opt = argparse_setup()
+    print(opt, '\n')
+    plotting_script(None, opt)
+    # interogateParams(None, opt)
+
+    # cleanup
+    if opt.root is not None and opt.delete_root:
+        rmtree(opt.root)
+
+if __name__ == '__main__':
+    # plot_xyz_gif()
+    plot_sc_contact_maps('/home/eric/dataset_test', samples = 91, ofolder = 'sc_contact/original', jobs = 8)
+    # plot_centroid_distance(parallel = True, samples = [34, 35, 36])
+    # update_result_tables('ContactGNN', 'GNN', 'sequence')
+    # update_result_tables('ContactGNNEnergy', 'GNN', 'energy')
+    # plot_combined_models('ContactGNN', [202, 203, 204])
+    # main()
