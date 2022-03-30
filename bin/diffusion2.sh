892df49d07c684901949970d0c04805d9076b886
ERSchultz <schultz-eric@hotmail.com>
2022-03-30 16:33:26 -0500

    job

--- bin/diffusion2.sh
+++ bin/diffusion2.sh
@@ -1,17 +1,18 @@
 #! /bin/bash
 #SBATCH --job-name=diffusion2
-#SBATCH --partition=bigmem2
+#SBATCH --partition=depablo
 #SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=10
+#SBATCH --ntasks-per-node=20
+#SBATCH --mem=0
 #SBATCH --output=logFiles/diffusion2.log
 #SBATCH --time=24:00:00
 
 dir="/project2/depablo/walt/michrom/project/chr_05/chr_05_02"
 odir="/project2/depablo/erschultz/michrom/project/chr_05/chr_05_02"
+jobs=15
 
 cd ~/sequences_to_contact_maps
 source activate python3.8_pytorch1.8.1_cuda10.2_2
 module load cmake
 
-python3 diffusion_analysis.py --dir $dir --odir $odir
+python3 diffusion_analysis.py --dir $dir --odir $odir --jobs $jobs
