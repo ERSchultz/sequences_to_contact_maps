4ff1642841ae602dbbf9e30e92b931388cac5540
ERSchultz <schultz-eric@hotmail.com>
2022-04-07 16:10:44 -0500

    job

--- bin/diffusion2.sh
+++ bin/diffusion2.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=diff2
 #SBATCH --partition=bigmem2
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=28
+#SBATCH --ntasks-per-node=20
 #SBATCH --mem=0
 #SBATCH --output=logFiles/diffusion2.log
 #SBATCH --time=24:00:00
@@ -11,9 +11,10 @@ dir="/project2/depablo/walt/michrom/project/chr_05/chr_05_02"
 odir="/project2/depablo/erschultz/michrom/project/chr_05/chr_05_02_copy2"
 jobs=20
 downSampling=12
+its=5
 
 cd ~/sequences_to_contact_maps
 source activate python3.8_pytorch1.8.1_cuda10.2_2
 module load cmake
 
-python3 diffusion_analysis.py --dir $dir --odir $odir --jobs $jobs --down_sampling $downSampling
+python3 diffusion_analysis.py --dir $dir --odir $odir --jobs $jobs --down_sampling $downSampling --its $its
--- diffusion_analysis.py
+++ diffusion_analysis.py
@@ -38,7 +38,7 @@ def getArgs(default_dir='/home/erschultz/dataset_test/samples/sample92'):
     parser.add_argument('--sparse_format', action='store_true',
                         help='True to store sc_contacts in sparse format')
     parser.add_argument('--down_sampling', type=int, default=5)
-    parser.add_argument('--its', type=int, default=5,
+    parser.add_argument('--its', type=int, default=2,
                         help='number of iterations')
 
     args = parser.parse_args()
