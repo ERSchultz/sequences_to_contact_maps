4f6d03bffea1234eb1f2a78eeacd8becd8c3a5d8
ERSchultz <schultz-eric@hotmail.com>
2022-04-18 13:48:38 -0500

    cleanup

--- bin/cleanup.sh
+++ bin/cleanup.sh
@@ -5,7 +5,56 @@
 #SBATCH --output=logFiles/cleanup.log
 #SBATCH --time=2:00:00
 
-cd ~/scratch-midway2/
+dir=/home/erschultz/sequences_to_contact_maps/results/Akita
+for i in $( seq 3 8 )
+do
+  cd "dir/${i}$"
+  rm *.pt
+done
 
+dir=/home/erschultz/sequences_to_contact_maps/results/DeepC
+for i in $( seq 7 18 )
+do
+  cd "dir/${i}$"
+  rm *.pt
+done
 
-rm -r dataset_01_17_22
+dir=/home/erschultz/sequences_to_contact_maps/results/ContactGNN
+for i in $( seq 1 228 )
+do
+  echo $i
+  cd "dir/${i}$"
+  rm *.pt
+done
+
+dir=/home/erschultz/sequences_to_contact_maps/results/GNNAutoencoder
+for i in $( seq 24 56 )
+do
+  echo $i
+  cd "dir/${i}$"
+  rm *.pt
+done
+
+dir=/home/erschultz/sequences_to_contact_maps/results/GNNAutoencoder2
+for i in $( seq 1 7 )
+do
+  echo $i
+  cd "dir/${i}$"
+  rm *.pt
+done
+
+dir=/home/erschultz/sequences_to_contact_maps/results/SequenceFCAutoencoder
+for i in $( seq 3 8 )
+do
+  echo $i
+  cd "dir/${i}$"
+  rm *.pt
+done
+
+dir=/home/erschultz/sequences_to_contact_maps/results/UNet
+for i in $( seq 1 39 )
+do
+  echo $i
+  cd "dir/${i}$"
+  rm *.pt
+done
