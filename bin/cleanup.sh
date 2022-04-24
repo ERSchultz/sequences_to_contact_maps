9794bae6b6ae232b22f9208408886ebb1666025e
ERSchultz <schultz-eric@hotmail.com>
2022-04-24 12:08:33 -0500

    cleanup

--- bin/cleanup.sh
+++ bin/cleanup.sh
@@ -11,7 +11,7 @@ cd "/home/erschultz/scratch-midway2/dataset_01_17_22/samples"
 for i in $( seq 1 4 )
 do
   echo $i
-  rm "sample${i}*" &
+  rm -r "sample${i}*" &
 done
 
 wait
@@ -23,7 +23,7 @@ cd "/home/erschultz/scratch-midway2/dataset_09_02_21/samples"
 for i in $( seq 1 4 )
 do
   echo $i
-  rm "sample${i}*" &
+  rm -r "sample${i}*" &
 done
 
 wait
