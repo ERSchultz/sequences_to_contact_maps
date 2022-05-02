b54584a08e5e468baa4544e1478365c09f2d5a59
ERSchultz <schultz-eric@hotmail.com>
2022-05-02 15:45:27 -0500

    uncomment gaussian

--- bin/diffusion.sh
+++ bin/diffusion.sh
@@ -9,7 +9,7 @@
 #SBATCH --mail-user=erschultz@uchicago.edu
 
 dir="/project2/depablo/walt/michrom/project/chr_05/chr_05_02"
-odir="/project2/depablo/erschultz/michrom/project/chr_05/chr_05_02_copy"
+odir="/project2/depablo/erschultz/michrom/project/chr_05/chr_05_02"
 jobs=20
 downSampling=12
 its=4
--- diffusion_analysis.py
+++ diffusion_analysis.py
@@ -44,7 +44,7 @@ def getArgs(default_dir='/home/erschultz/dataset_test/samples/sample92'):
     parser.add_argument('--jobs', type=int, default=15)
     parser.add_argument('--sparse_format', action='store_true',
                         help='True to store sc_contacts in sparse format')
-    parser.add_argument('--down_sampling', type=int, default=1)
+    parser.add_argument('--down_sampling', type=int, default=2)
     parser.add_argument('--its', type=int, default=1,
                         help='number of iterations')
     parser.add_argument('--chunk_size', type=int, default=500,
@@ -71,6 +71,7 @@ def getArgs(default_dir='/home/erschultz/dataset_test/samples/sample92'):
     args.log_file = open(args.log_file_path, 'a')
 
     print(args, file = args.log_file)
+    print(args)
     return args
 
 class Updater():
@@ -464,9 +465,9 @@ class GaussianProcessing():
 
     def inner(self, ifile, ofile):
         y = np.load(ifile)
-        # y = triu_to_full(y, self.m)
-        # y = gaussian_filter(y, sigma = 4)
-        # y = y[np.triu_indices(self.m)]
+        y = triu_to_full(y, self.m)
+        y = gaussian_filter(y, sigma = 4)
+        y = y[np.triu_indices(self.m)]
         np.save(ofile, y)
 
 def xyz_diffusion():
