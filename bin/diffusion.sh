91d08cc1811513d5271703db013662c470b1f404
Eric Schultz <erschultz@uchicago.edu>
2022-08-24 20:07:10 -0500

    job

--- bin/diffusion.sh
+++ bin/diffusion.sh
@@ -6,6 +6,7 @@
 #SBATCH --time=24:00:00
 
 odir="/home/erschultz/sequences_to_contact_maps/single_cell_nagano_2017"
+scratch='/scratch/midway2/erschultz'
 dir="${odir}/samples"
 k=2
 experimental='true'
@@ -16,5 +17,6 @@ downSampling=10
 plot='true'
 preprocessingMode='sparsity_filter'
 
+conda activate python3.9_pytorch1.9_cuda10.2
 
 python3 /home/erschultz/sequences_to_contact_maps/diffusion_analysis.py --dir $dir --odir $odir --k $k --experimental $experimental --jobs $jobs --metric $metric --its $its --down_sampling $downSampling --plot $plot --preprocessing_mode $preprocessingMode
