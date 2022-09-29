07c84f50c5ec938eac752cec5efb8c459dbe6ac9
Eric Schultz <erschultz@uchicago.edu>
2022-09-29 11:31:43 -0500

    job

--- bin/cleanup.sh
+++ bin/cleanup.sh
@@ -10,3 +10,16 @@ dir="/home/erschultz/scratch-midway2"
 cd $dir
 
 rm -r dataset_04*
+
+dir="/project2/depablo/erschultz"
+cd $dir
+
+dataset="${dir}/${dataset_09_21_21/samples}"
+for i in 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25
+do
+  cd "${dataset}/sample${i}"
+  rm -r k_means* &
+  rm -r PCA* &
+  rm -r nmf* &
+  wait
+done
