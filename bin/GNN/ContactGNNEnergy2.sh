83eec0367eae8c5f3aebed8799401d7fc325ea6c
Eric Schultz <erschultz@uchicago.edu>
2022-10-19 17:10:36 -0500

    sbatch missing account

--- bin/GNN/ContactGNNEnergy2.sh
+++ bin/GNN/ContactGNNEnergy2.sh
@@ -2,6 +2,7 @@
 #SBATCH --job-name=CGNNE2
 #SBATCH --output=logFiles/ContactGNNEnergy2.out
 #SBATCH --time=1-24:00:00
+#SBATCH --account=pi-depablo
 #SBATCH --partition=depablo-gpu
 #SBATCH --gres=gpu:1
 #SBATCH --nodes=1
--- bin/GNN/wrapper.sh
+++ bin/GNN/wrapper.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 
 
-for i in 6 7
+for i in 1 2 3
+# 6 7
  # 4 5
 do
   sbatch ~/sequences_to_contact_maps/bin/GNN/ContactGNNEnergy${i}.sh
