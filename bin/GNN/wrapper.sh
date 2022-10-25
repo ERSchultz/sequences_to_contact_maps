787195ed65b9bd50644a70dbb6ad7f7a1225264e
Eric Schultz <erschultz@uchicago.edu>
2022-10-25 15:26:26 -0500

    jobs

--- bin/GNN/wrapper.sh
+++ bin/GNN/wrapper.sh
@@ -1,10 +1,8 @@
 #! /bin/bash
 
 
-for i in 2 8
-# 1 3 - TODO midway3
-# 5 6 7 - running (poorly)
- # 4 - starting
+for i in 5 6 7 8
+# 4 running on midway3
 do
   sbatch ~/sequences_to_contact_maps/bin/GNN/ContactGNNEnergy${i}.sh
 done
