d91afe0f26f8a8aae9d91ec1cc47a67adde83085
Eric Schultz <erschultz@uchicago.edu>
2022-10-24 21:34:44 -0500

    job

--- bin/GNN/ContactGNNEnergy4.sh
+++ bin/GNN/ContactGNNEnergy4.sh
@@ -2,40 +2,49 @@
 #SBATCH --job-name=CGNNE4
 #SBATCH --output=logFiles/ContactGNNEnergy4.out
 #SBATCH --time=1-24:00:00
+#SBATCH --account=pi-depablo
 #SBATCH --partition=depablo-gpu
 #SBATCH --gres=gpu:1
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=10
 #SBATCH --mem-per-cpu=2000
-#SBATCH --mail-type=FAIL
+#SBATCH --mail-type=END
 #SBATCH --mail-user=erschultz@uchicago.edu
 
 cd ~/sequences_to_contact_maps
 
 source bin/GNN/GNN_fns.sh
 source activate python3.9_pytorch1.9_cuda10.2
+source activate python3.9_pytorch1.9
 
 rootName='ContactGNNEnergy4' # change to run multiple bash files at once
-dirname="/project2/depablo/erschultz/dataset_09_30_22"
+dirname="/project/depablo/erschultz/dataset_09_30_22"
 m=1024
-messagePassing='GAT'
-preTransforms='degree-ContactDistance-GeneticDistance-DiagonalParameterDistance_80'
-mlpModelID='80'
+messagePassing='weighted_GAT'
+preTransforms='degree-ContactDistance-GeneticDistance'
+mlpModelID='none'
 useEdgeAttr='true'
 hiddenSizesList='8-8-8'
 EncoderHiddenSizesList='100-100-64'
 updateHiddenSizesList='100-100-64'
 numHeads=8
 
+outputMode='energy_sym_diag'
+yPreprocessing='log'
 yLogTransform='none'
 sparsifyThreshold='none'
-yPreprocessing='log'
-# no log/ln transform, no sparsification, using log preprocessing
+yNorm='mean'
+headArchitecture2='fc-fill'
+headHiddenSizesList='1000-1000-1000-1000-1000-1000-1024'
+scratch='/scratch/midway3/erschultz'
 
-id=191
+# fc fill
+# Run on midway3
+
+id=199
 for lr in 1e-4
 do
   train
   id=$(( $id + 1 ))
 done
-python3 ~/sequences_to_contact_maps/utils/clean_directories.py --data_folder $dirname --GNN_file_name $rootName --scratch $scratch > clean.log
+python3 ~/sequences_to_contact_maps/utils/clean_directories.py --data_folder $dirname --GNN_file_name $rootName --scratch $scratch
