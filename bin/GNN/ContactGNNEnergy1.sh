21954de4e0d08e3b0bcdd4639e6a07874bfbdfcb
Eric Schultz <erschultz@uchicago.edu>
2022-10-27 15:13:28 -0500

    job

--- bin/GNN/ContactGNNEnergy1.sh
+++ bin/GNN/ContactGNNEnergy1.sh
@@ -20,8 +20,8 @@ source activate python3.9_pytorch1.9
 rootName='ContactGNNEnergy1' # change to run multiple bash files at once
 dirname="/project2/depablo/erschultz/dataset_09_30_22"
 m=1024
-messagePassing='GAT'
-preTransforms='degree-ContactDistance-GeneticDistance_norm'
+messagePassing='weighted_GAT'
+preTransforms='degree-ContactDistance_norm-GeneticDistance_norm'
 mlpModelID='none'
 useEdgeAttr='true'
 hiddenSizesList='8-8-8'
@@ -35,10 +35,11 @@ yNorm='mean'
 headArchitecture='fc'
 headHiddenSizesList='1000-1000-1000-1000-1000-1000-1024'
 
-# first attempt at predicting only diag with GNN, baseline arch
+# attempt at predicting only diag with GNN, norm = mean, contact_distance_norm
+# repeat of 214 with split = False
 
 
-id=210
+id=216
 for lr in 1e-4
 do
   train
