13c10a43dee9b9b22b50be36d4aff5c7e10c4935
Eric Schultz <erschultz@uchicago.edu>
2022-11-07 15:15:45 -0600

    jobs

--- bin/GNN/ContactGNNEnergy9.sh
+++ bin/GNN/ContactGNNEnergy9.sh
@@ -18,13 +18,13 @@ source activate python3.9_pytorch1.9_cuda10.2
 source activate python3.9_pytorch1.9
 
 rootName='ContactGNNEnergy9' # change to run multiple bash files at once
-dirname="/project/depablo/erschultz/dataset_09_30_22"
+dirname="/project2/depablo/erschultz/dataset_09_30_22"
 m=1024
 messagePassing='weighted_GAT'
 preTransforms='constant-ContactDistance-GeneticDistance_norm'
 mlpModelID='none'
 useEdgeAttr='true'
-hiddenSizesList='8-8-8-8'
+hiddenSizesList='8-8-8'
 EncoderHiddenSizesList='1000-1000-64'
 updateHiddenSizesList='1000-1000-64'
 numHeads=8
@@ -36,7 +36,6 @@ headArchitecture='bilinear'
 headArchitecture2='fc-fill'
 headHiddenSizesList='1000-1000-1000-1000-1000-1000-1024'
 KR='true'
-scratch='/scratch/midway3/erschultz'
 useScratch='false'
 
 
