d246e1412062116f7fcd3ce886ff518166bf87a0
ERSchultz <schultz-eric@hotmail.com>
2023-03-02 14:07:24 -0600

    'changes'

--- bin/GNN/ContactGNNEnergy4.sh
+++ bin/GNN/ContactGNNEnergy4.sh
@@ -18,7 +18,7 @@ source activate python3.9_pytorch1.9_cuda10.2
 source activate python3.9_pytorch1.9
 
 rootName='ContactGNNEnergy4' # change to run multiple bash files at once
-dirname="/project/depablo/erschultz/dataset_03_01_23"
+dirname="/project2/depablo/erschultz/dataset_03_01_23"
 m=512
 preTransforms='constant-ContactDistance-GeneticDistance_norm-AdjPCs_8'
 hiddenSizesList='8-8-8-8'
