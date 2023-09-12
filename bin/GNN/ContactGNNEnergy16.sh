1b799ac21c8ceb4572c905d6b00d13ded0355b11
Eric Randall Schultz <erschultz@midway3-login3.rcc.local>
2023-09-12 08:26:25 -0500

    results

--- bin/GNN/ContactGNNEnergy16.sh
+++ bin/GNN/ContactGNNEnergy16.sh
@@ -3,7 +3,7 @@
 #SBATCH --output=logFiles/ContactGNNEnergy16.out
 #SBATCH --time=24:00:00
 #SBATCH --account=pi-depablo
-#SBATCH --partition=depablo-gpu
+#SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=16
