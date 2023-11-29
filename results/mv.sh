e7dadd352e6ae2b2a90b0bf13ea26f9a6cb0f618
Eric Randall Schultz <erschultz@midway3-login3.rcc.local>
2023-11-29 09:19:08 -0600

    results

--- results/mv.sh
+++ results/mv.sh
@@ -1,9 +1,9 @@
 #! bin/bash
 dir="/home/erschultz/sequences_to_contact_maps/results/ContactGNNEnergy"
-for i in {612..619}
+for i in {620..621}
 do
 	i_dir="${dir}/${i}"
-	# scp "${i_dir}/model.pt" erschultz@10.150.30.72:${i_dir}/model.pt
+	scp "${i_dir}/model.pt" erschultz@10.150.30.72:${i_dir}/model.pt
 	# scp "${i_dir}/model_early_stop.pt" erschultz@10.150.30.72:${i_dir}/model_early.pt
 	# rm "${i_dir}/model.pt"
 	# rm "${i_dir}/model_early_stop.pt"
