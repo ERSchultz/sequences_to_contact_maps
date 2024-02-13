77a2739ff394f20983ef23a36cda41a10324b493
Eric Randall Schultz <erschultz@midway3-login3.rcc.local>
2024-02-13 13:44:35 -0600

    results

--- results/shrink_results.py
+++ results/shrink_results.py
@@ -6,7 +6,7 @@ def shrink():
     dir = '/home/erschultz/sequences_to_contact_maps/results/ContactGNNEnergy'
     assert osp.exists(dir), f'{dir} does not exist'
     print(dir)
-    for i in [651, 652, 653, 654]:
+    for i in [649]:
         i_dir = osp.join(dir, str(i))
         print(i_dir)
         model_dir = osp.join(i_dir, 'model.pt')
