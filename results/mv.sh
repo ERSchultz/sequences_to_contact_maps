d7e7ffb36e6b4732bb9a9d52d67362fd63932ff0
Eric Randall Schultz <erschultz@midway3-login3.rcc.local>
2024-02-16 16:03:09 -0600

    results

--- results/mv.sh
+++ results/mv.sh
@@ -1,6 +1,6 @@
 #! bin/bash
 dir="/home/erschultz/sequences_to_contact_maps/results/ContactGNNEnergy"
-for i in 657 658
+for i in 665 667
 do
 	i_dir="${dir}/${i}"
 	scp "${i_dir}/model.pt" erschultz@10.150.30.72:${i_dir}/model.pt
--- results/shrink_results.py
+++ results/shrink_results.py
@@ -6,7 +6,7 @@ def shrink():
     dir = '/home/erschultz/sequences_to_contact_maps/results/ContactGNNEnergy'
     assert osp.exists(dir), f'{dir} does not exist'
     print(dir)
-    for i in [657, 658]:
+    for i in [665, 667]:
         i_dir = osp.join(dir, str(i))
         print(i_dir)
         model_dir = osp.join(i_dir, 'model.pt')
